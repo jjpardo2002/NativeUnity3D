e50b27eacbd7cced0e84d2513facb4bbbff925cd
Johlver Pardo <jjpardo2002@gmail.com>
2020-08-08 16:51:38 -0500

    Native commit

---
+++ Assets/LeerDato.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using TMPro;
+public class LeerDato : MonoBehaviour
+{
+#if UNITY_IOS
+        const string Test ="__Internal";
+#else
+    const string Test = "Test";
+#endif
+    [DllImport(Test)]
+    private static extern System.IntPtr Reversar(string n);
+
+    public TMP_InputField word; //Word to reverse
+    public TMP_Text result; // Result final before reverse
+
+    void Start()
+    {
+        
+    }
+    public void reverseText()
+    {
+        result.text= Marshal.PtrToStringAnsi(Reversar(word.text));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FinalNative/NativeDll_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,62 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.IntPtr LeerDato::Reversar(System.String)
+extern void LeerDato_Reversar_mF4D611C47055FBE4D6D8A827ECEA21EA87F96172 ();
+// 0x00000002 System.Void LeerDato::Start()
+extern void LeerDato_Start_m994A77126E0342F5A1C09D0BFEFD25260E91CDAD ();
+// 0x00000003 System.Void LeerDato::reverseText()
+extern void LeerDato_reverseText_m4F500C1AF3FF7DAA18611569994C70102B8CF2C5 ();
+// 0x00000004 System.Void LeerDato::Update()
+extern void LeerDato_Update_m8027856716D03A0F30B0DCD711AFFFEFE3B24233 ();
+// 0x00000005 System.Void LeerDato::.ctor()
+extern void LeerDato__ctor_m9A66A55274D4F7231324F6B917936B78093615D0 ();
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	LeerDato_Reversar_mF4D611C47055FBE4D6D8A827ECEA21EA87F96172,
+	LeerDato_Start_m994A77126E0342F5A1C09D0BFEFD25260E91CDAD,
+	LeerDato_reverseText_m4F500C1AF3FF7DAA18611569994C70102B8CF2C5,
+	LeerDato_Update_m8027856716D03A0F30B0DCD711AFFFEFE3B24233,
+	LeerDato__ctor_m9A66A55274D4F7231324F6B917936B78093615D0,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	24,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
